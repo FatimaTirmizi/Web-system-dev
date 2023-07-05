4b88cec41ed4f04c3ed0c8616e0a55c500547619
Fatima Ali Tirmizi <fatimaalitirmizi12@gmail.com>
2023-07-05 15:24:27 +0500

    Real and img

---
+++ src/ComplexNo.java
@@ -0,0 +1,24 @@
+public class ComplexNo {
+
+    private int realPart;
+
+    private int imgPart;
+
+    public ComplexNo(){
+
+        realPart = 0;
+        imgPart = 0;
+
+    }
+
+    public ComplexNo(int realPart, int imgPart) {
+        this.realPart = realPart;
+        this.imgPart = imgPart;
+    }
+
+    public void print(){
+        System.out.println("("+realPart+"+"+imgPart+"i"+")");
+    }
+}
+
+
